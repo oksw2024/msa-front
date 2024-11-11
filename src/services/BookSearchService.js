f8902d7f52537fa73f4f3c5942dcfafacf1ebe0b
oksw2024 <oksw205@gmail.com>
2024-11-11 17:50:47 +0900

    Add files via upload

--- src/services/BookSearchService.js
+++ src/services/BookSearchService.js
@@ -1,12 +1,12 @@
 // BookSearchService.js
 
-export const fetchBooks = async (type, query) => {
+export const fetchBooks = async (type, query, page = 0, size = 5) => {
     const baseUrl = 'https://data4library.kr/api/srchBooks';
     const apiKey = '246bc9a1a2ea4ba78b5ada1b16a0ba7e43537ef40b0427f80013629f7b593a86';
     const formattedQuery = query.split(' ').join(';'); // 검색어 공백을 ;로 변환
 
     // 기본 URL과 API 키, 형식 지정
-    let url = `${baseUrl}?authKey=${apiKey}&format=json&pageSize=5&pageNo=1`;
+    let url = `${baseUrl}?authKey=${apiKey}&format=json&pageSize=${size}&pageNo=${page + 1}`;
 
     // 검색 옵션에 따라 URL에 파라미터 추가
     if (type === 'title') {
@@ -22,13 +22,25 @@ export const fetchBooks = async (type, query) => {
     try {
         const response = await fetch(url);
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error(`Network response was not ok: ${response.status}`);
         }
         const data = await response.json();
         console.log('API Response Data:', data); // API 응답 출력
-        return data.response?.docs || [];
+
+        // 결과 데이터에서 문서 리스트와 총 검색 결과 개수 반환
+        const docs = data.response?.docs || [];
+        const numFound = data.response?.numFound || 0; // 총 검색 결과 개수
+        const totalPages = Math.ceil(numFound / size); // 총 페이지 수 계산
+
+        return {
+            books: docs,
+            totalPages, // 총 페이지 수 반환
+        };
     } catch (error) {
         console.error('Fetch error:', error);
-        return [];
+        return {
+            books: [],
+            totalPages: 0,
+        };
     }
 };
