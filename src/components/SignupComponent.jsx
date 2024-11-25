8472ca7e8797e34f3a6657fe3f453836ae2b8277
oksw2024 <oksw205@gmail.com>
2024-11-25 20:35:37 +0900

    회원가입, 로그인 수정

--- src/components/SignupComponent.jsx
+++ src/components/SignupComponent.jsx
@@ -7,48 +7,57 @@ export default function SignupComponent() {
   const [username, setUsername] = useState('');
   const [loginId, setLoginId] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordTest, setPasswordTest] = useState('');
   const [email, setEmail] = useState('');
 
-  const [message, setMessage] = useState('');
+  const [nameMessage, setNameMessage] = useState('');
+  const [idMessage, setIdMessage] = useState('');
+  const [passwordMessage, setPasswordMessage] = useState('');
+  const [checkMessage, setCheckMessage] = useState('');
+  const [emailMessage, setEmailMessage] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordTest, setShowPasswordTest] = useState(false);
   const navigate = useNavigate();
 
   const handleBack = async () => {
     navigate('/');
   }
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  }
+
+  const togglePasswordTestVisivility = () => {
+    setShowPasswordTest(!showPasswordTest);
+  }
+
   const handleSignup = async () => {
+    setNameMessage("");
+    setIdMessage("");
+    setPasswordMessage("");
+    setCheckMessage("");
+    setEmailMessage("");
 
     // 공백 체크
-    if (!username.trim() || !loginId.trim() || !password.trim() || !email.trim()) {
-      setMessage("값을 입력해주세요");
+    if (!username.trim()) {
+      setNameMessage("사용자 이름을 입력해주세요.");
       return;
-    }
-    
-    // 사용자 이름 특수문자 체크
-    const usernameRegex = /^[가-힣a-zA-Z0-9]+$/;
-    if (!usernameRegex.test(username)) {
-      setMessage("사용자 이름은 한글, 영문자, 숫자로 입력해주세요.");
+    } else if (!loginId.trim()) {
+      setIdMessage("아이디를 입력해주세요.");
       return;
-    }
-
-    // loginId 영어와 숫자만 체크
-    const loginIdRegex = /^[a-zA-Z0-9]{4,20}$/;
-    if (!loginIdRegex.test(loginId)) {
-      setMessage("아이디는 영문자와 숫자로 4자 이상 20자 이하로 입력해주세요.");
+    } else if (!password.trim()) {
+      setPasswordMessage("비밀번호를 입력해주세요.");
       return;
-    }
-
-    // 비밀번호 제한사항: 최소 8자, 반드시 특수문자 포함
-    const passwordRegex = /^(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    if (!passwordRegex.test(password)) {
-      setMessage("비밀번호는 최소 8자 이상이어야 하며, 반드시 특수문자를 포함해야 합니다");
+    } else if (!passwordTest.trim()) {
+      setCheckMessage("비밀번호 확인을 입력해주세요.");
+      return;
+    } else if (!email.trim()) {
+      setEmailMessage("이메일을 입력해주세요.");
       return;
     }
 
-    // 이메일 형식 체크
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-      setMessage("유효한 이메일 주소를 입력해주세요");
+    if (password !== passwordTest) {
+      setCheckMessage("비밀번호가 일치하지 않습니다.");
       return;
     }
 
@@ -71,6 +80,46 @@ export default function SignupComponent() {
     }
   };
 
+  const handleBlur = (field) => {
+    if (field === 'username') {
+      const usernameRegex = /^[가-힣a-zA-Z0-9]+$/;
+      if (!username.trim()) {
+        setNameMessage('사용자 이름을 입력해주세요.');
+      } else if (!usernameRegex.test(username)){
+        setNameMessage("사용자 이름은 한글, 영문자, 숫자로 입력해주세요.");
+      } else {
+        setNameMessage('');
+      }
+    } else if (field === 'loginId') {
+      const loginIdRegex = /^[a-zA-Z0-9]{4,20}$/;
+      if (!loginId.trim()) {
+        setIdMessage('아이디를 입력해주세요.');
+      } else if (!loginIdRegex.test(loginId)) {
+        setIdMessage('아이디는 영문자와 숫자로 4자 이상 20자 이하로 입력해주세요.');
+      } else {
+        setIdMessage('');
+      }
+    } else if (field === 'password') {
+      const passwordRegex = /^(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+      if (!password.trim()) {
+        setPasswordMessage('비밀번호를 입력해주세요.');
+      } else if (!passwordRegex.test(password)) {
+        setPasswordMessage('비밀번호는 최소 8자 이상이어야 하며, 반드시 특수문자를 포함해야 합니다.');
+      } else {
+        setPasswordMessage('');
+      }
+    } else if (field === 'email') {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!email.trim()) {
+        setEmailMessage('이메일을 입력해주세요.');
+      } else if (!emailRegex.test(email)) {
+        setEmailMessage('유효한 이메일 주소를 입력해주세요.');
+      } else {
+        setEmailMessage('');
+      }
+    }
+  };
+
   return (
     <div className='center'>
       <h2>회원가입</h2>
@@ -78,29 +127,58 @@ export default function SignupComponent() {
         type="text"
         placeholder="사용자 이름"
         value={username}
+        onBlur={() => handleBlur('username')}
         onChange={(e) => setUsername(e.target.value)}
       />
+      {nameMessage && <p>{nameMessage}</p>}
       <input
         type="text"
         placeholder="아이디"
         value={loginId}
+        onBlur={() => handleBlur('loginId')}
         onChange={(e) => setLoginId(e.target.value)}
       />
-      <input
-        type="password"
-        placeholder="비밀번호"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+      {idMessage && <p>{idMessage}</p>}
+      <div className='password-container'>
+        <input
+          type={showPassword ? "text" : "password"}
+          placeholder="비밀번호"
+          value={password}
+          onBlur={() => handleBlur('password')}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          type="button"
+          onClick={togglePasswordVisibility}
+        >{showPassword ? "숨기기" : "보기"}</button>
+      </div>
+      {passwordMessage && <p>{passwordMessage}</p>}
+      <div className='password-container'>
+        <input
+          type={showPasswordTest ? "text" : "password"}
+          placeholder="비밀번호 확인"
+          value={passwordTest}
+          onBlur={() => handleBlur('passwordTest')}
+          onChange={(e) => setPasswordTest(e.target.value)}
+        />
+        <button
+          type="button"
+          onClick={togglePasswordTestVisivility}
+        >{showPasswordTest ? "숨기기" : "보기"}</button>
+      </div>
+      {checkMessage && <p>{checkMessage}</p>}
       <input
         type="text"
         placeholder="이메일"
         value={email}
+        onBlur={() => handleBlur('email')}
         onChange={(e) => setEmail(e.target.value)}
       />
-      <button onClick={handleSignup}>가입</button>
-      <button onClick={handleBack}>취소</button>
-      {message && <p>{message}</p>}
+      {emailMessage && <p>{emailMessage}</p>}
+      <div className='button-container'>
+        <button onClick={handleSignup}>가입</button>
+        <button onClick={handleBack}>취소</button>
+      </div>
     </div>
   );
 }
