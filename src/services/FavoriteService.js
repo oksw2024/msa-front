68b14c1f3cd93ff40ff7476581a27316244a0c76
oksw2024 <oksw205@gmail.com>
2024-12-02 10:27:44 +0900

    로그인 만료 추가

--- src/services/FavoriteService.js
+++ src/services/FavoriteService.js
@@ -6,7 +6,6 @@ const API_BASE_URL = 'http://localhost:8080/api/favorites';
 export const getFavorites = async (accessToken) => {
     if (!accessToken) {
         console.error('Access token is missing');
-        throw new Error('Access token is required to fetch favorites');
     }
 
     try {
@@ -16,7 +15,45 @@ export const getFavorites = async (accessToken) => {
         return response.data;
     } catch (error) {
         console.error('Error fetching favorites:', error);
-        throw error;
+
+        if (error.response && error.response.status == 401) {
+            try {
+                const refreshToken = localStorage.getItem('refreshToken');
+      
+                if (!refreshToken) {
+                  setMessage('Failed to find refresh token.');
+                  return;
+                }
+      
+                const refreshResponse = await axios.get('http://localhost:8080/api/v1/auth/refresh', {
+                  headers: { REFRESH_TOKEN: refreshToken },
+                });
+      
+                const newAccessToken = refreshResponse.data;
+                localStorage.setItem('accessToken', newAccessToken);
+                setMessage('Token refreshed successfully.');
+                console.log('new accessToken:', newAccessToken);
+      
+                const retryResponse = await axios.get(`${API_BASE_URL}/get`, {
+                    headers: { Authorization: `Bearer ${newAccessToken}` },
+                });
+
+                console.log('retry response : ', retryResponse);
+      
+                return retryResponse.data;
+            } catch (refreshError) {
+                console.error('Failed to refresh token:', refreshError.response);
+      
+                if(refreshError.response?.status === 403) {
+                  setMessage('다시 로그인해주세요');
+                  localStorage.removeItem('accessToken');
+                  localStorage.removeItem('refreshToken');
+                  navigate('/login');
+                }
+            }
+        } else {
+            setMessage('Failed to call favorite books.');
+        }
     }
 };
 
@@ -31,13 +68,61 @@ export const addFavorite = async (accessToken, bookDetails) => {
         bookImageURL: bookImageUrl,
     } = bookDetails;
 
-    return axios.post(
-        `${API_BASE_URL}/add`,
-        { bookIsbn, bookTitle, author, publisher, publicationYear, bookImageUrl },
-        {
-            headers: { Authorization: `Bearer ${accessToken}` },
+    try {
+        const response = await axios.post(
+            `${API_BASE_URL}/add`,
+            { bookIsbn, bookTitle, author, publisher, publicationYear, bookImageUrl },
+            {
+                headers: { Authorization: `Bearer ${accessToken}` },
+            }
+        );
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching favorites add:', error);
+
+        if (error.response && error.response.status == 401) {
+            try {
+                const refreshToken = localStorage.getItem('refreshToken');
+      
+                if (!refreshToken) {
+                  setMessage('Failed to find refresh token.');
+                  return;
+                }
+      
+                const refreshResponse = await axios.get('http://localhost:8080/api/v1/auth/refresh', {
+                  headers: { REFRESH_TOKEN: refreshToken },
+                });
+      
+                const newAccessToken = refreshResponse.data;
+                localStorage.setItem('accessToken', newAccessToken);
+                setMessage('Token refreshed successfully.');
+                console.log('new accessToken:', newAccessToken);
+      
+                const retryResponse = await axios.post(
+                    `${API_BASE_URL}/add`,
+                    { bookIsbn, bookTitle, author, publisher, publicationYear, bookImageUrl },
+                    {
+                        headers: { Authorization: `Bearer ${newAccessToken}` },
+                    }
+                );
+
+                console.log('retry response : ', retryResponse);
+      
+                return retryResponse.data;
+            } catch (refreshError) {
+                console.error('Failed to refresh token:', refreshError.response);
+      
+                if(refreshError.response?.status === 403) {
+                  setMessage('다시 로그인해주세요');
+                  localStorage.removeItem('accessToken');
+                  localStorage.removeItem('refreshToken');
+                  navigate('/login');
+                }
+            }
+        } else {
+            setMessage('Failed to add favorite books.');
         }
-    );
+    }
 };
 
 // 즐겨찾기 삭제
@@ -51,7 +136,44 @@ export const removeFavorite = async (accessToken, bookIsbn) => {
         });
     } catch (error) {
         console.error('Error removing favorite:', error);
-        throw error;
+        
+        if (error.response && error.response.status == 401) {
+            try {
+                const refreshToken = localStorage.getItem('refreshToken');
+      
+                if (!refreshToken) {
+                  setMessage('Failed to find refresh token.');
+                  return;
+                }
+      
+                const refreshResponse = await axios.get('http://localhost:8080/api/v1/auth/refresh', {
+                  headers: { REFRESH_TOKEN: refreshToken },
+                });
+      
+                const newAccessToken = refreshResponse.data;
+                localStorage.setItem('accessToken', newAccessToken);
+                setMessage('Token refreshed successfully.');
+                console.log('new accessToken:', newAccessToken);
+      
+                await axios.delete(`${API_BASE_URL}/remove`, {
+                    headers: {
+                        Authorization: `Bearer ${newAccessToken}`,
+                    },
+                    data: { bookIsbn },
+                });
+            } catch (refreshError) {
+                console.error('Failed to refresh token:', refreshError.response);
+      
+                if(refreshError.response?.status === 403) {
+                  setMessage('다시 로그인해주세요');
+                  localStorage.removeItem('accessToken');
+                  localStorage.removeItem('refreshToken');
+                  navigate('/login');
+                }
+            }
+        } else {
+            setMessage('Failed to delete favorite books.');
+        }
     }
 };
 
@@ -63,10 +185,50 @@ export const getRecommendedBooks = async (accessToken) => {
                 Authorization: `Bearer ${accessToken}`,
             },
         });
-        console.log("Recommendations API Response:", response.data); // 디버깅용
+
         return response.data;
     } catch (error) {
         console.error("Error fetching recommended books:", error.response?.data || error.message);
-        throw error;
+
+        if (error.response && error.response.status == 401) {
+            try {
+                const refreshToken = localStorage.getItem('refreshToken');
+      
+                if (!refreshToken) {
+                  setMessage('Failed to find refresh token.');
+                  return;
+                }
+      
+                const refreshResponse = await axios.get('http://localhost:8080/api/v1/auth/refresh', {
+                  headers: { REFRESH_TOKEN: refreshToken },
+                });
+      
+                const newAccessToken = refreshResponse.data;
+                localStorage.setItem('accessToken', newAccessToken);
+                setMessage('Token refreshed successfully.');
+                console.log('new accessToken:', newAccessToken);
+      
+                const retryResponse = await axios.get(`${API_BASE_URL}/recommendations`, {
+                    headers: {
+                        Authorization: `Bearer ${newAccessToken}`,
+                    },
+                });
+
+                console.log('retry response : ', retryResponse);
+      
+                return retryResponse.data;
+            } catch (refreshError) {
+                console.error('Failed to refresh token:', refreshError.response);
+      
+                if(refreshError.response?.status === 403) {
+                  setMessage('다시 로그인해주세요');
+                  localStorage.removeItem('accessToken');
+                  localStorage.removeItem('refreshToken');
+                  navigate('/login');
+                }
+            }
+        } else {
+            setMessage('Failed to call recommended books.');
+        }
     }
 };
